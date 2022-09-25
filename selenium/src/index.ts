f43fa0a1eb0fe910414d1e9ad4aa6e410d926912
hgsgtk <kazuki@autify.com>
2022-09-25 09:26:56 +0900

    impl: google search

--- selenium/src/index.ts
+++ selenium/src/index.ts
@@ -1,1 +1,13 @@
 import {Builder, Browser, By, Key, until} from 'selenium-webdriver';
+
+(async function googleSearch() {
+    const driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+    try {
+        await driver.get('http://www.google.com/ncr');
+        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+        await driver.quit();
+    }
+})();
