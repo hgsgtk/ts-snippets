ecc4bd4fe810a224132aa5004bf54fffd95c41b9
hgsgtk <kazuki@autify.com>
2022-10-08 11:18:45 +0900

    small webdriverio with TypeScript

--- wdio/simple/src/index.ts
+++ wdio/simple/src/index.ts
@@ -1,1 +1,29 @@
-console.log("hello")
+import { remote } from "webdriverio";
+
+(async() => {
+    // Standalone mode
+    // ref: https://webdriver.io/docs/setuptypes#standalone-mode
+    const client = await remote({
+        logLevel: 'trace',
+        capabilities: {
+            browserName: 'chrome'
+        }
+    })
+    // Protocol Binding mode
+    // ref: https://webdriver.io/docs/setuptypes#package-api
+    // const client = await WebDriver.newSession({
+    //     capabilities: {
+    //         browserName: 'chrome'
+    //     }
+    // })
+    
+    await client.navigateTo('https://www.google.com/ncr')
+
+    const searchInput = await client.findElement('css selector', '#lst-ib')
+    await client.elementClick(searchInput['element-6066-11e4-a52e-4f735466cecf'])
+
+    console.log(await client.getTitle())
+
+    await client.deleteSession()
+})();
+
