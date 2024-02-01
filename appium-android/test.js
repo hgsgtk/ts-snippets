633d7f3ce216854d7fd89e51e988f6dbec884edd
hgsgtk <kazuki@autify.com>
2024-02-01 15:22:50 +0900

    fix capabilities

--- appium-android/test.js
+++ appium-android/test.js
@@ -24,7 +24,15 @@ const opts = {
     path: '/wd/hub',
     port: 4723,
     capabilities: {
-        // Must be an empty object.
+        // Capabilities: https://docs.aws.amazon.com/devicefarm/latest/testgrid/techref-support.html
+        platformName: 'Android',
+        browserName: 'Chrome',
+
+        "appium:deviceName": 'Android Emulator',
+        "appium:automationName": 'UiAutomator2',
+        // An unknown server-side error occurred while processing the command. Original error: Unable to find an active device or emulator with OS 2.1. The following are available:
+        //  emulator-5554 (14) <- 14
+        "appium:platformVersion": '14',
     }
 }
 
